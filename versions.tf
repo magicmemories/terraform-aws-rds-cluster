6898d9daefb4b28e6a8531ac1899b2557eb8ea7f
VenkataMutyala <venkata.mutyala@outlook.com>
2020-12-29 15:57:44 -0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
-provider "aws" {
-  version = "~> 3.0"
+terraform {
+  required_version = ">= 0.13.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.1.15"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+  }
 }
